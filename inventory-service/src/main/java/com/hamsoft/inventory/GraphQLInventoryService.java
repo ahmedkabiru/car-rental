d17cb4dfeffa6f9ea81d9497c41804e47cb1bde6
ahka <ahmed.kabiru@spreadgroup.com>
2024-11-20 12:18:11 +0100

    Implement graphql inventory endpoint

---
+++ inventory-service/src/main/java/com/hamsoft/inventory/GraphQLInventoryService.java
@@ -0,0 +1,41 @@
+package com.hamsoft.inventory;
+
+import com.hamsoft.inventory.database.CarInventory;
+import com.hamsoft.inventory.model.Car;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.graphql.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@GraphQLApi
+public class GraphQLInventoryService {
+
+    @Inject
+    CarInventory carInventory;
+
+
+    @Query
+    @Description("List of Cars")
+    public List<Car> cars() {
+        return  carInventory.getCars();
+    }
+
+    @Mutation
+    public Car createCar(Car car) {
+        car.id = CarInventory.id.incrementAndGet();
+        carInventory.getCars().add(car);
+        return car;
+    }
+
+    @Mutation
+    public boolean remove(String  licensePlateNumber) {
+        List<Car> cars = carInventory.getCars();
+        Optional<Car> carToRemove  = cars.stream().filter(car -> car.licensePlateNumber.equals(licensePlateNumber)).findAny();
+        if(carToRemove.isPresent()) {
+          return carInventory.getCars().remove(carToRemove.get());
+        }else {
+            return false;
+        }
+    }
+}
---
+++ inventory-service/src/main/java/com/hamsoft/inventory/model/Car.java
@@ -0,0 +1,9 @@
+package com.hamsoft.inventory.model;
+
+public class Car {
+
+    public Long id;
+    public String licensePlateNumber;
+    public String manufacturer;
+    public String model;
+}
